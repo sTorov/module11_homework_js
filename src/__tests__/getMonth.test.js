9519674ba8a79c4415d71c2e819e3143ece157bd
sTorov <105942932+sTorov@users.noreply.github.com>
2023-01-16 00:17:39 +0300

    Update getMonth.test.js

--- src/__tests__/getMonth.test.js
+++ src/__tests__/getMonth.test.js
@@ -1,6 +1,9 @@
 import { getMonth } from "../getMonth.js";
 
-describe("Tests for function getMonth", () => {    
+describe("Tests for function getMonth", () => {
+    const regexIncorrectDataType = /^Неверный тип данных! Необходимо передать число!$/;
+    const regexIncorrectValue = /^Значение должно быть в диапозоне от 1 до 12!$/;
+
     it("must return correct month name", () => {
         expect(getMonth(1)).toBe("январь");
         expect(getMonth(4)).toBe("апрель");
@@ -9,12 +12,12 @@ describe("Tests for function getMonth", () => {
         expect(getMonth(12)).toBe("декабрь");
     }),
     it("check throw exception", () => {
-        expect(() => getMonth()).toThrow(/^Неверный тип данных! Необходимо передать число!$/);
-        expect(() => getMonth(0)).toThrow(/^Значение должно быть в диапозоне от 1 до 12!$/);
-        expect(() => getMonth('test')).toThrow(/^Неверный тип данных! Необходимо передать число!$/);
-        expect(() => getMonth(13)).toThrow(/^Значение должно быть в диапозоне от 1 до 12!$/);
-        expect(() => getMonth(null)).toThrow(/^Неверный тип данных! Необходимо передать число!$/);
-        expect(() => getMonth(NaN)).toThrow(/^Значение должно быть в диапозоне от 1 до 12!$/);
-        expect(() => getMonth(Infinity)).toThrow(/^Значение должно быть в диапозоне от 1 до 12!$/);
+        expect(() => getMonth()).toThrow(regexIncorrectDataType);
+        expect(() => getMonth(0)).toThrow(regexIncorrectValue);
+        expect(() => getMonth('test')).toThrow(regexIncorrectDataType);
+        expect(() => getMonth(13)).toThrow(regexIncorrectValue);
+        expect(() => getMonth(null)).toThrow(regexIncorrectDataType);
+        expect(() => getMonth(NaN)).toThrow(regexIncorrectValue);
+        expect(() => getMonth(Infinity)).toThrow(regexIncorrectValue);
     })
 });
